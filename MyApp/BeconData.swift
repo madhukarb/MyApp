0ee5a7281cdb5bafcc72b2390e838dbe8e04929f
Madhukar <Madhukar@Madhukars-MacBook-Pro.local>
2017-10-28 19:31:47 -0600

    Touch ID enabled, login errors are now iOS Alerts

---
+++ MyApp/BeconData.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  MyApp
+//
+//  Created by Madhukar Bommala on 10/28/17.
+//  Copyright © 2017 Madhukar. All rights reserved.
+//
+
+import Foundation
+
+struct BeconData{
+    var beaconUID: String?
+    var major: Int
+    var minor: Int
+    var inRange: Bool = false
+}
---
+++ MyApp/LoggedInUser.swift
@@ -0,0 +1,13 @@
+//
+//  LoggedInUser.swift
+//  MyApp
+//
+//  Created by Madhukar Bommala on 10/28/17.
+//  Copyright © 2017 Madhukar. All rights reserved.
+//
+
+import Foundation
+
+var LoggedInUser = UserStruct()
+//var InRangeBeaconDate = BeconData()
+
